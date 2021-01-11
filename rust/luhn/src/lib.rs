5fce1333552fc71b8607098f2afe3517ad598dcb
Dalton Maahs <dalton.maahs@yourbind.com>
2021-01-11 03:24:30 -0600

    luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,4 +1,13 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(sum, c), d| {
+            d.to_digit(10)
+                .map(|n| if c % 2 == 1 { n * 2 } else { n })
+                .map(|n| if n > 9 { n - 9 } else { n })
+                .map(|n| (n + sum, c + 1))
+        })
+        .map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
 }
