5c51140185681f88066a035ee8b13552df542176
Dalton Maahs <dalton.maahs@yourbind.com>
2021-01-29 15:01:28 -0600

    high scores

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,38 +1,30 @@
-use std::collections::HashSet;
 #[derive(Debug)]
-pub struct HighScores<'a> {
-    scores: &'a[u32],
-    latest: Option<u32>,
+pub struct HighScores {
+    scores: Vec<u32>,
 }
 
-impl<'a> HighScores<'a> {
-    pub fn new(scores: &'a[u32]) -> Self {
-        HighScores {
-            scores,
-            latest: None,
+impl HighScores {
+    pub fn new(scores: &[u32]) -> Self {
+        Self {
+            scores: scores.iter().cloned().collect(),
         }
     }
 
     pub fn scores(&self) -> &[u32] {
-        &self.scores
+        &self.scores[..]
     }
 
     pub fn latest(&self) -> Option<u32> {
-        self.latest
+        self.scores.last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        if self.scores.len() > 0 {
-            Some(self.scores[0])
-        } else {
-            None
-        }
+        self.scores.iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        let scores: HashSet<u32> = self.scores.iter()
-            .fold(HashSet::new(), |mut acc, &s| { acc.insert(s); acc });
-        
-        unimplemented!()
+        let mut scores = self.scores.clone();
+        scores.sort_unstable();
+        scores.iter().cloned().rev().take(3).collect()
     }
 }
