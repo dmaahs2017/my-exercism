2735ad6262a0e8105073ac06687fdd2342204471
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-02 09:53:35 -0600

    clever

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,27 +1,12 @@
 pub fn abbreviate(phrase: &str) -> String {
-    phrase
-        .replace("-", " ")
-        .split_whitespace()
-        .map(|w| {
-            let word = {
-                if !w.chars().filter(|c| c.is_alphabetic()).all(|c| c.is_ascii_uppercase()) {
-                    let mut c = w.chars();
-                    match c.next() {
-                        None => String::new(),
-                        Some(letter) => letter.to_ascii_uppercase().to_string() + c.as_str(),
-                    }
-                } else {
-                    let w = w.to_ascii_lowercase();
-                    let mut c = w.chars();
-                    match c.next() {
-                        None => String::new(),
-                        Some(letter) => letter.to_ascii_uppercase().to_string() + c.as_str(),
-                    }
-                }
-            };
+    phrase.split(|c: char| c.is_whitespace() || c == '-' || c == '_')
+        .flat_map(|word| {
             word.chars()
-                .filter(|c| c.is_ascii_uppercase())
-                .collect::<String>()
+                .take(1)
+                .chain(word.chars()
+                    .skip_while(|c| c.is_uppercase())
+                    .filter(|c| c.is_uppercase()))
         })
-        .collect()
+        .collect::<String>()
+        .to_uppercase()
 }
