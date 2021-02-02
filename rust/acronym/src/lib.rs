42539527f86419de94d52401dfa563f2c28c81c5
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-02 09:49:16 -0600

    acronym

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,27 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    phrase
+        .replace("-", " ")
+        .split_whitespace()
+        .map(|w| {
+            let word = {
+                if !w.chars().filter(|c| c.is_alphabetic()).all(|c| c.is_ascii_uppercase()) {
+                    let mut c = w.chars();
+                    match c.next() {
+                        None => String::new(),
+                        Some(letter) => letter.to_ascii_uppercase().to_string() + c.as_str(),
+                    }
+                } else {
+                    let w = w.to_ascii_lowercase();
+                    let mut c = w.chars();
+                    match c.next() {
+                        None => String::new(),
+                        Some(letter) => letter.to_ascii_uppercase().to_string() + c.as_str(),
+                    }
+                }
+            };
+            word.chars()
+                .filter(|c| c.is_ascii_uppercase())
+                .collect::<String>()
+        })
+        .collect()
 }
