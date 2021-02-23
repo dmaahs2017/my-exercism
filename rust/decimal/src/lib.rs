adb4e230689c1055dd87c2e2261c820af9e18612
Dalton Maahs <dalton.maahs@yourbind.com>
2021-02-22 23:58:53 -0600

    decimal

--- rust/decimal/src/lib.rs
+++ rust/decimal/src/lib.rs
@@ -1,4 +1,7 @@
+//! Arbitrary precision decimal arthimetic type
+#![deny(missing_docs)]
 #![feature(str_split_once)]
+
 
 use num_bigint::{BigInt, ToBigInt};
 use num_integer::Integer;
@@ -7,13 +10,14 @@ use std::fmt;
 use std::ops::{Add, Mul, Sub};
 
 /// Type implementing arbitrary-precision decimal arithmetic
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Debug)]
 pub struct Decimal {
     numerator: BigInt,
     denominator: BigInt,
 }
 
 impl Decimal {
+    /// Create new arbitrary-precision decimal.
     pub fn new(numerator: BigInt, denominator: BigInt) -> Self {
         let gcd = numerator.gcd(&denominator);
         Self {
@@ -22,15 +26,24 @@ impl Decimal {
         }
     }
 
+    /// Create a new Decimal from a string. Supports positive and negative numbers.
+    /// ```
+    /// use decimal::Decimal;
+    /// Decimal::try_from("-1.1").unwrap();
+    /// ```
     pub fn try_from(input: &str) -> Option<Decimal> {
         if let Some(dec_pos) = input.find(".") {
             let mut numerator: BigInt = input[..dec_pos].parse().ok()?;
+            let decimal_part = &input[dec_pos + 1..];
 
-            let post_decimal = &input[dec_pos + 1..];
-            let denominator = 10.to_bigint()?.pow(post_decimal.len() as u32);
+            let denominator = 10.to_bigint()?.pow(decimal_part.len() as u32);
 
-            numerator = numerator * &denominator + post_decimal.parse::<BigInt>().ok()?;
-
+            numerator = numerator * &denominator;
+            if input.find("-").is_some() {
+                numerator -= decimal_part.parse::<BigInt>().ok()?;
+            } else {
+                numerator += decimal_part.parse::<BigInt>().ok()?;
+            }
             return Some(Self::new(numerator, denominator));
         }
 
@@ -43,13 +56,7 @@ impl Mul for Decimal {
     fn mul(self, rhs: Self) -> Self::Output {
         let numerator = &self.numerator * &rhs.numerator;
         let denominator = self.denominator * rhs.denominator;
-
-        let gcd = numerator.gcd(&denominator);
-
-        Self {
-            numerator: numerator / &gcd,
-            denominator: denominator / &gcd,
-        }
+        Self::new(numerator, denominator)
     }
 }
 
@@ -58,7 +65,6 @@ impl Add for Decimal {
     fn add(self, rhs: Self) -> Self::Output {
         let denominator = &self.denominator * &rhs.denominator;
         let numerator = &self.numerator * rhs.denominator + &rhs.numerator * self.denominator;
-
         Self::new(numerator, denominator)
     }
 }
@@ -82,22 +88,11 @@ impl cmp::Ord for Decimal {
     fn cmp(&self, other: &Self) -> cmp::Ordering {
         let lhs = &self.numerator * &other.denominator;
         let rhs = &other.numerator * &self.denominator;
-
         lhs.cmp(&rhs)
     }
 }
 
 impl fmt::Display for Decimal {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(
-            f,
-            "{} / {}",
-            self.numerator.to_string(),
-            self.denominator.to_string()
-        )
-    }
-}
-impl fmt::Debug for Decimal {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         write!(
             f,
