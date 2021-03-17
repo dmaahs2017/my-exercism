9d47d385aa496a1cd4cee7b8ba4e159c0a342ba4
Dalton Maahs <dalton.maahs@yourbind.com>
2021-03-16 20:32:27 -0500

    triangles

---
+++ rust/triangle/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct Triangle<T> {
+    sides: [T; 3],
+}
+
+impl<T> Triangle<T>
+where
+    T: PartialEq + PartialOrd + Default + std::ops::Add<Output = T> + Copy,
+{
+    pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
+        // check for 0 values
+        for &s in sides.iter() {
+            if s == T::default() {
+                return None;
+            }
+        }
+
+        // check for 2 sides that aren't >= the remaining side
+        if sides[0] + sides[1] < sides[2]
+            || sides[0] + sides[2] < sides[1]
+            || sides[1] + sides[2] < sides[0]
+        {
+            return None;
+        }
+
+        Some(Triangle { sides })
+    }
+
+    pub fn is_equilateral(&self) -> bool {
+        self.sides[0] == self.sides[1]
+            && self.sides[0] == self.sides[2]
+            && self.sides[1] == self.sides[2]
+    }
+
+    pub fn is_scalene(&self) -> bool {
+        self.sides[0] != self.sides[1]
+            && self.sides[0] != self.sides[2]
+            && self.sides[1] != self.sides[2]
+    }
+
+    pub fn is_isosceles(&self) -> bool {
+        !self.is_equilateral() && !self.is_scalene()
+    }
+}
