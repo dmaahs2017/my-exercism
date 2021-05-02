1b5ee14e752e106a978aec251441be1bf8cc9979
Dalton Maahs <dalton.maahs@yourbind.com>
2021-05-02 01:03:18 -0500

    iter 2

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -1,45 +1,30 @@
 pub struct Luhn {
-    digits: Option<Vec<u8>>,
+    s: String,
 }
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        self.digits.as_ref().map_or(false, |digits| {
-            let (sum, count) = digits
-                .iter()
-                .rev()
-                .cloned()
-                .fold((0, 0), |(sum, c), mut d| {
-                    if c % 2 == 1 {
-                        d *= 2
-                    };
-                    if d > 9 {
-                        d -= 9
-                    };
-                    (d + sum, c + 1)
-                });
-            sum % 10 == 0 && count > 1
-        })
+        self.s
+            .chars()
+            .rev()
+            .filter(|c| !c.is_whitespace())
+            .try_fold((0, 0), |(sum, c), d| {
+                d.to_digit(10)
+                    .map(|n| if c % 2 == 1 { n * 2 } else { n })
+                    .map(|n| if n > 9 { n - 9 } else { n })
+                    .map(|n| (n + sum, c + 1))
+            })
+            .map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
     }
 }
 
-/// Here is the example of how the From trait could be implemented
-/// for the &str type. Naturally, you can implement this trait
-/// by hand for the every other type presented in the test suite,
-/// but your solution will fail if a new type is presented.
-/// Perhaps there exists a better solution for this problem?
 impl<T> From<T> for Luhn
 where
     T: std::fmt::Display,
 {
     fn from(input: T) -> Self {
         Self {
-            digits: input
-                .to_string()
-                .chars()
-                .filter(|c| !c.is_whitespace())
-                .map(|c| c.to_digit(10).map(|d| d as u8))
-                .collect(),
+            s: input.to_string(),
         }
     }
 }
