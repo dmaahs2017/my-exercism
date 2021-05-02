4a3fac4432c628c7a15d63eb24d9daba1318d46e
Dalton Maahs <dalton.maahs@yourbind.com>
2021-05-02 01:07:53 -0500

    iter 1

---
+++ rust/luhn-trait/src/lib.rs
@@ -0,0 +1,22 @@
+pub trait Luhn {
+    fn valid_luhn(&self) -> bool;
+}
+
+impl<T> Luhn for T
+where
+    T: std::fmt::Display,
+{
+    fn valid_luhn(&self) -> bool {
+        self.to_string()
+            .chars()
+            .rev()
+            .filter(|c| !c.is_whitespace())
+            .try_fold((0, 0), |(sum, c), d| {
+                d.to_digit(10)
+                    .map(|n| if c % 2 == 1 { n * 2 } else { n })
+                    .map(|n| if n > 9 { n - 9 } else { n })
+                    .map(|n| (n + sum, c + 1))
+            })
+            .map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
+    }
+}
