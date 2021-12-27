1bdee232aeaf45f9448a99a7f75aac4a19dd60c6
Dalton Maahs <dalton.maahs@yourbind.com>
2021-12-27 16:57:35 -0600

    som

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,12 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let is_factor_of = |n: u32, factors: &[u32]| -> bool {
-        factors.iter().fold(false, |acc, &factor| {
-            if factor == 0 {
-                return acc;
-            }
-            acc || n % factor == 0
-        })
-    };
-
-    (1..limit).filter(|&i| is_factor_of(i, factors)).sum()
+    (1..limit)
+        .filter(|&i| factors.iter().any(|&f| f != 0 && i % f == 0))
+        .sum()
 }
