7349715833788fb144847b41a843afe152608f27
Dalton Maahs <dalton.maahs@yourbind.com>
2021-12-28 08:15:51 -0600

    prime

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,4 +1,24 @@
-use primes;
-pub fn factors(n: u64) -> Vec<u64> {
-    primes::factors(n)
+pub fn factors(mut n: u64) -> Vec<u64> {
+    let mut result = Vec::new();
+
+    let mut iter = std::iter::successors(Some(2), |&count| {
+        if n == 1 {
+            return None;
+        }
+
+        if count as f64 > (n as f64).sqrt() {
+            result.push(n);
+            return None;
+        }
+
+        if n % count == 0 {
+            result.push(count);
+            n /= count;
+            Some(2)
+        } else {
+            Some(count + 1)
+        }
+    });
+    while let Some(_) = iter.next() {}
+    return result;
 }
