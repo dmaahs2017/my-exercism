072c10aa55ada795003eac09c4da1c10e5b7345a
Dalton Maahs <dalton.maahs@yourbind.com>
2022-01-03 22:56:55 -0600

    allergies

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,35 @@
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+pub struct Allergies {
+    score: u8,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, EnumIter)]
+#[repr(u8)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self { score: score as u8 }
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        dbg!(self.score >> *allergen as u8);
+        (self.score >> *allergen as u8) % 2 == 1
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        Allergen::iter()
+            .filter(|a| self.is_allergic_to(a))
+            .collect()
+    }
+}
