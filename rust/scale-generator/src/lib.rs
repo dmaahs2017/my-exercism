79230ca44471581001e480efe6676bc057075905
Dalton Maahs <dalton.maahs@yourbind.com>
2022-02-23 13:41:15 -0600

    improved

--- rust/scale-generator/src/lib.rs
+++ rust/scale-generator/src/lib.rs
@@ -1,7 +1,11 @@
-#[derive(Debug)]
+#[warn(clippy::unwrap_used, clippy::expect_used)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
-    Unknown,
+    InvalidNote(String),
+    Undeterminiate,
 }
+
+type ScaleResult<T> = Result<T, Error>;
 
 type Notes = [&'static str; 12];
 const SHARPS: Notes = [
@@ -12,81 +16,96 @@ const FLATS: Notes = [
     "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
 ];
 
-/// Usize = half step
-enum Interval {
-    Chromatic,
-    Pattern(Vec<usize>),
+/// Note is u8 because the note should never exceed 11
+type Note = u8;
+pub struct Scale {
+    flats: bool,
+    notes: Vec<Note>,
 }
 
-pub struct Scale {
-    notes: Notes,
-    root: usize, // index of NOTES
-    interval: Interval,
+fn note_string(note: Note, flats: bool) -> String {
+    if flats {
+        FLATS[note as usize].to_string()
+    } else {
+        SHARPS[note as usize].to_string()
+    }
+}
+
+fn string_to_note(note_str: &str) -> ScaleResult<Note> {
+    let invalid_note_fn = || Error::InvalidNote(note_str.to_string());
+    let mut byte_iter = note_str.as_bytes().iter();
+
+    let mut sum = match byte_iter
+        .next()
+        .ok_or_else(invalid_note_fn)?
+        .to_ascii_uppercase()
+    {
+        b @ (b'A' | b'B') => (b - b'A') as i8 * 2,
+        b @ (b'C'..=b'E') => (b - b'A') as i8 * 2 - 1,
+        b @ (b'F' | b'G') => (b - b'A') as i8 * 2 - 2,
+        _ => return Err(invalid_note_fn()),
+    };
+
+    sum += if let Some(b) = byte_iter.next() {
+        match b {
+            b'#' => 1,
+            b'b' => -1,
+            _ => return Err(invalid_note_fn()),
+        }
+    } else {
+        0
+    };
+
+    if byte_iter.next().is_some() {
+        return Err(invalid_note_fn());
+    }
+
+    // A flat should be half step 11
+    if sum < 0 {
+        sum += 12;
+    }
+
+    Ok(sum as Note)
 }
 
 impl Scale {
     // tonic = start note
-    pub fn new(tonic: &str, intervals: &str) -> Result<Scale, Error> {
-        let notes = if use_flats(tonic) { FLATS } else { SHARPS };
+    pub fn new(tonic: &str, intervals: &str) -> ScaleResult<Scale> {
+        let flats = use_flats(tonic);
+        let root = string_to_note(tonic)?;
 
-        let root = get_root_idx(tonic, notes);
-
-        let interval = Interval::Pattern(
-            intervals
-                .chars()
-                .map(|c| match c {
-                    'M' => 2,
-                    'm' => 1,
-                    'A' => 3,
+        let notes = intervals
+            .as_bytes()
+            .iter()
+            .fold((root, vec![root]), |(mut note, mut acc), b| {
+                match b {
+                    b'm' => note += 1,
+                    b'M' => note += 2,
+                    b'A' => note += 3,
                     _ => unreachable!(),
-                })
-                .collect(),
-        );
+                }
+                note %= 12;
+                acc.push(note);
+                (note, acc)
+            })
+            .1;
 
-        Ok(Scale {
-            notes,
-            root,
-            interval,
-        })
+        Ok(Self { flats, notes })
     }
 
-    pub fn chromatic(tonic: &str) -> Result<Scale, Error> {
-        let notes = if use_flats(tonic) { FLATS } else { SHARPS };
+    pub fn chromatic(tonic: &str) -> ScaleResult<Self> {
+        let flats = use_flats(tonic);
+        let root = string_to_note(tonic)?;
+        let notes = (0..12).cycle().skip(root as usize).take(13).collect();
 
-        let root = get_root_idx(tonic, notes);
-
-        Ok(Scale {
-            notes,
-            root,
-            interval: Interval::Chromatic,
-        })
+        Ok(Scale { flats, notes })
     }
 
     pub fn enumerate(&self) -> Vec<String> {
-        match &self.interval {
-            Interval::Chromatic => self
-                .notes
-                .iter()
-                .cycle()
-                .skip(self.root)
-                .take(13)
-                .map(|s| s.to_string())
-                .collect(),
-            Interval::Pattern(pattern_interval) => {
-                let mut iter = self.notes.iter().cycle().skip(self.root);
-                pattern_interval
-                    .iter()
-                    .map(|interval| {
-                        let note = iter.next().unwrap().to_string();
-                        for _ in 1..*interval {
-                            iter.next();
-                        }
-                        note
-                    })
-                    .chain(std::iter::once(self.notes[self.root].to_string()))
-                    .collect()
-            }
-        }
+        self.notes
+            .iter()
+            .map(|note| note_string(*note, self.flats))
+            .collect()
     }
 }
 
@@ -95,19 +114,71 @@ fn use_flats(tonic: &str) -> bool {
         "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb",
     ];
 
-    return dbg!(flat_tonics.contains(&tonic));
+    flat_tonics.contains(&tonic)
 }
 
-fn get_root_idx(tonic: &str, notes: Notes) -> usize {
-    notes
-        .iter()
-        .enumerate()
-        .find_map(|(i, t)| {
-            if *t.to_ascii_uppercase() == tonic.to_ascii_uppercase() {
-                Some(i)
-            } else {
-                None
-            }
-        })
-        .unwrap()
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn string_to_note_works() {
+        let str_note = "A";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 0);
+
+        let str_note = "A#";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 1, "sharps work");
+
+        let str_note = "Bb";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 1, "flats work");
+
+        let str_note = "B";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 2);
+
+        let str_note = "C";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 3);
+
+        let str_note = "C#";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 4);
+
+        let str_note = "Eb";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 6);
+
+        let str_note = "E";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 7);
+
+        let str_note = "F";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 8);
+
+        let str_note = "F#";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 9);
+
+        let str_note = "G";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 10);
+
+        let str_note = "G#";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 11);
+
+        let str_note = "Ab";
+        let note = string_to_note(str_note).unwrap();
+        assert_eq!(note, 11);
+    }
+
+    #[test]
+    fn new_test() {
+        let s = Scale::new("B", "mMA").unwrap();
+        assert_eq!(s.notes, vec![2, 3, 5, 8]);
+    }
 }
